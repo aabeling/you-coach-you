f2a034afdfc69f9ccd40e11c5465431c940c9f4b
Achim Abeling <achimabeling@web.de>
2020-12-19 10:12:50 +0100

    first try with nativescript-texttospeech

--- src/app/speaker-example/speaker-example.component.ts
+++ src/app/speaker-example/speaker-example.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TNSTextToSpeech, SpeakOptions } from 'nativescript-texttospeech';
 
 @Component({
   selector: 'ns-speaker-example',
@@ -17,5 +18,22 @@ export class SpeakerExampleComponent implements OnInit {
    */
   saySomething() {
     console.log("say something");
+
+    let TTS = new TNSTextToSpeech();
+    //TTS.getAvailableLanguages().
+    let speakOptions: SpeakOptions = {
+        text: 'Hallo Achim du alter Sack'
+    }
+
+    // Call the `speak` method passing the SpeakOptions object
+    TTS.speak(speakOptions).then(() => {
+        console.log("fine");
+        alert("everything is fine");
+    }, (err) => {
+    console.log("failed");
+        alert("something failed: " + err);
+    });
+
+    console.log("saying something finished");
   }
 }
