6f289ae1fd9a6903b0ca2ade73109fb288306ab8
Achim Abeling <achimabeling@web.de>
2020-12-21 09:32:18 +0100

    added plugin nativescript-insomnia to keep the screen active

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { keepAwake, allowSleepAgain } from "nativescript-insomnia";
 
 @Component({
     selector: "ns-app",
     templateUrl: "./app.component.html"
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+    ngOnInit(): void {    
+        
+        /* keep the screen from getting inactive */
+        keepAwake().then(function() {
+            console.log("Insomnia is active");
+        })
+    } 
+
+}
